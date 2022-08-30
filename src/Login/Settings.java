955bc97e65607d10bee47bd45e676b70d95b622d
alinaprotsyk <alinaprotsyk2108@gmail.com>
2022-08-29 22:47:16 -0400

    settings updated by alina

--- src/Login/Settings.java
+++ src/Login/Settings.java
@@ -1,12 +1,12 @@
 package Login;
 
 public class Settings {
-<<<<<<< HEAD
+
     // some settings - alina
     // some settings - alina
-=======
+
 
     // I changed some settings here - Helen
     // I changed some settings here - Helen
->>>>>>> develop
+
 }
